b49da43c360a0d2143caa89ea995b5d560ac606e
ZiBullet <zamolidinziadulaev@gmail.com>
2022-05-26 09:46:57 +0500

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const form = document.forms.asker
-const needInputs = form.querySelectorAll('.needed')
+const needInputs = form.querySelectorAll('.nes')
 const name = form.querySelector('#name')
+const spanName = form.querySelector('.spanName')
+const spans = form.querySelectorAll('span')
+
+
 
 form.onsubmit = (event) => {
     event.preventDefault()
     let errs = []
     needInputs.forEach(input => {
-        if (input.classList.contains('invalid') || input.length === 0) {
-            errs.push('error')
-            input.style.border = '2px solid red'
-        }
+            spans.forEach(span => {
+                if (input.value.length === 0) {
+                    input.style.border = 'var(--border-err)'
+                    errs.push('error')
+                    span.style.color = 'red'
+                } 
+            })
     })
-    return errs.length === 0 ? submit() : 'smth went wrong'
+    
+    return errs.length === 0 ? submit() : console.log('smth went wrong');
 }
+
 
 function submit() {
     let user = {}
